267f4ce3644c619faf2c49bf234c9bb5821f1a77
TarsioGomides <tarsiogomideslima@hotmail.com>
2025-04-21 03:58:18 -0300

    wip: testing

--- infra/global/backend/providers.tf
+++ infra/global/backend/providers.tf
@@ -6,7 +6,13 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "terraform-state-wheather-alerts-and-insights"
+    key            = "dev/terraform.tfstate"
+    region         = "sa-east-1"
+    dynamodb_table = "terraform-locks-wheather-alerts-and-insights"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
